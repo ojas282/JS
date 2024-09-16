59b822d2382de37bedb0dac52cc3cbeb982b136c
ojas282 <ojaskumargupta28@gmail.com>
2024-09-16 15:33:31 +0530

    01

---
+++ 01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail  = "ojas@google.com"
+var accountPassword = "123"
+accountCity = "Jaipur"
+let accountState 
+
+// accountId = 2
+accountEmail = "og@gmail.com"
+accountPassword = "345"
+accountCity = "Lucknow"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
