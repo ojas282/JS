cb4dc180789fcaeeddba8f5b96d84270b9d18ea7
ojas282 <ojaskumargupta28@gmail.com>
2024-09-17 10:29:52 +0530

    Execution context

--- IIFE.js
+++ IIFE.js
@@ -1,6 +1,29 @@
 // Immediately Invoked Function Expression (IIFE)
 
-function chai(){
+// function chai(){
+//   console.log('DB CONNECTED')
+// }
+// chai()
+
+//to immediately invoke the function 
+// function chai is a named IIFE
+(function chai(){
   console.log('DB CONNECTED')
-}
-chai()
+})();
+
+// 1st -> ()--> function definition second -->()->execution
+/* Avoid Polluting the Global Scope: JavaScript has function-level scope, meaning that variables declared within functions are not accessible outside of them. By using an IIFE, you create a new scope, preventing variables inside the function from leaking into the global scope.*/
+
+/*	Private Variables and Methods: Variables and functions inside an IIFE are isolated from the global environment. This allows you to create private variables or methods that cannot be accessed or modified from outside the <function className=""></function> */
+
+( function aurchai(){
+  console.log(`DB connected to`)
+})();
+
+// if we dont use ; after the first function definition then the execution will not end there and hence the second function wont start before the first ends
+
+// simple iife or unnamed iife
+((name)=>{
+  console.log(`name is ${name}`)
+})("ojas")
+
---
+++ JS_ExecutionContext.js
@@ -0,0 +1,12 @@
+// Execution Context is how th JS will run and execute the code
+// JS runs code in 2 phases
+/*
+1st -- GLOBAL EXECUTION CONTEXT
+2nd-- FUNCTION EXECUTION CONTEXT
+EVAL EXECUTION CONTEXT
+
+
+MEMORY CREATION PHASE 1: PHASE 1 memory is allocated for varibles only
+EXECUTION PHASE : PHASE 2 it executes all the steps like +, -, *, /
+*/
+
