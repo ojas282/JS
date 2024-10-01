0f454e19744e1d1b9d9773ce4830033bf1be6cd6
ojas282 <ojaskumargupta28@gmail.com>
2024-10-01 14:16:31 +0530

    new project

---
+++ JS2/DOM/Projects/WebPageColorChanger/index.js
@@ -0,0 +1,58 @@
+const button = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+const texts = document.querySelectorAll('h1')
+const h2s = document.querySelectorAll('h2')
+console.log(button)
+
+button.forEach(function (button) {
+  button.addEventListener('click', function(e){
+    console.log(e)
+    console.log(e.target)
+    if(e.target.id == 'grey'){
+      body.style.background = e.target.id
+      texts.forEach(function(each){
+        each.style.color = "white"
+      })
+      h2s.forEach(function(each){
+        each.style.color = "white"
+      })
+
+    }
+    if(e.target.id == 'white'){
+      body.style.background = e.target.id
+      texts.forEach(function(each){
+        each.style.color = "black"
+      })
+      h2s.forEach(function(each){
+        each.style.color = "black"
+      })
+    }
+    if(e.target.id == 'blue'){
+      body.style.background = e.target.id
+      texts.forEach(function(each){
+        each.style.color = "white"
+      })
+      h2s.forEach(function(each){
+        each.style.color = "white"
+      })
+    }
+    if(e.target.id == 'yellow'){
+      body.style.background = e.target.id
+      texts.forEach(function(each){
+        each.style.color = "black"
+      })
+      h2s.forEach(function(each){
+        each.style.color = "black"
+      })
+    }
+    if(e.target.id == 'purple'){
+      body.style.background = e.target.id
+      texts.forEach(function(each){
+        each.style.color = "white"
+      })
+      h2s.forEach(function(each){
+        each.style.color = "white"
+      })
+    }
+  })
+})
